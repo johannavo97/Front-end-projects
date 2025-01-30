5d869e79381a76a9789a447821fc76ad4c37f9fa
Johanna Vo <thanhtruc2009@gmail.com>
2025-01-29 16:33:18 -0800

    finish

--- Speech-To-Text/script.js
+++ Speech-To-Text/script.js
@@ -1,41 +1,69 @@
+
 const resultElement = document.getElementById("result");
 let recognition;
 
-function startRecording() {
-    if('webkitSpeechRecognition' in window){ 
-        recognition = new webkitSpeechRecognition();
+
+function startConverting(){
+
+    if('webkitSpeechRecognition' in window){
+        recognition  = new webkitSpeechRecognition();
         setupRecognition(recognition);
         recognition.start();
     }
-
 }
+
 
 function setupRecognition(recognition){
+
     recognition.continuous = true;
+
     recognition.interimResults = true;
+
     recognition.lang = 'en-US';
-    recognition.onresult = function(event) { 
-        const {finalTranscript, interTranscript} = processResult(event.results);
+    
+    recognition.onresult = function(event){
+
+        const {finalTranscript , interTranscript } = processResult(event.results);
+    
         resultElement.innerHTML = finalTranscript + interTranscript;
+        
+    }
+
+
 }
 
+
 function processResult(results){
+
+
     let finalTranscript = '';
     let interTranscript = '';
+
     for(let i = 0; i < results.length; i++){
+
         let transcript = results[i][0].transcript;
         transcript.replace("\n","<br>");
+
         if(results[i].isFinal){
+
             finalTranscript += transcript;
-        } else {
+
+        }else{
+
             interTranscript += transcript;
         }
+
     }
+
     return {finalTranscript, interTranscript}
 
 }
 
-function stopRecording() {
-    if(recognition)
+
+function stopConverting(){
+
+    if(recognition){
         recognition.stop();
+    }
+
 }
